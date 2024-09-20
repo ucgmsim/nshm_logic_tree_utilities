6a7ab0c3dfe5909c005cd61047bcdd8afd6d37b7
Andrew Ridden-Harper <andrew.ridden-harper@canterbury.ac.nz>
2024-09-20 13:55:35 +1200

    introduced bug fix

--- logic_tree_tools.py
+++ logic_tree_tools.py
@@ -592,7 +592,7 @@ def get_logic_tree_pairs_for_individual_ground_motion_models(
         If more than one tectonic_region_type is included in any tectonic_region_type_set.
     """
 
-    if len(tectonic_region_type_sets[0]) == 1:
+    if len(tectonic_region_type_sets[0]) > 1:
         raise ValueError(
             "Only one tectonic_region_type can be included in each tectonic_region_type_set passed to this function."
         )
